7c48137463f0af2faadadc752468119d8b1501fa
Garrett Robinson <garrett@pressfreedomfoundation.org>
2014-05-15 08:10:34 -0700

    Revert "check PYTHONPATH in test runner"
    This reverts commit 72edb5fcfe82904d771efb24a7fcebf6040def2b.

--- securedrop/test.sh
+++ securedrop/test.sh
@@ -6,8 +6,7 @@ if [ $(which vagrant) ] ; then
     echo ""
 fi
 
-echo "PYTHONPATH: $PYTHONPATH"
-export PYTHONPATH=$PYTHONPATH:./tests
+export PYTHONPATH=./tests
 
 # -f makes unittest fail fast, so we can use && to avoid burying test failures
 python -m unittest -fv tests.unit_tests && python -m unittest -fv tests.functional.submit_and_retrieve_message #&& python -m unittest -fv tests.functional.submit_and_retrieve_file
